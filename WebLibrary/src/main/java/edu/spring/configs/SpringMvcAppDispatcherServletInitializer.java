1adcdb3c218275588d8f41d11a9ef7b0117a9315
tysaneno <tysaneno@student.21-school.ru>
2023-07-07 19:55:20 +0300

    WebLibrary: refactor use rus lang. Need add validator

--- WebLibrary/src/main/java/edu/spring/configs/SpringMvcAppDispatcherServletInitializer.java
+++ WebLibrary/src/main/java/edu/spring/configs/SpringMvcAppDispatcherServletInitializer.java
@@ -1,7 +1,11 @@
 package edu.spring.configs;
 
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -26,7 +30,19 @@ public class SpringMvcAppDispatcherServletInitializer extends
   @Override
   public void onStartup(ServletContext servletContext) throws ServletException {
     super.onStartup(servletContext);
+    registerCharacterEncodingFilter(servletContext);
     registerHiddenFieldFilter(servletContext);
+  }
+
+  private void registerCharacterEncodingFilter(ServletContext aContext) {
+    EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+    characterEncodingFilter.setEncoding("UTF-8");
+    characterEncodingFilter.setForceEncoding(true);
+
+    FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
+    characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
   }
 
   private void registerHiddenFieldFilter(ServletContext aContext) {
